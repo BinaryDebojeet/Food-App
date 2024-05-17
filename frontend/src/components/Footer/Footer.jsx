16e1b3b1b421637c16081c008af0c88fba02cbdb
Angshu Das <angshudas191@gmail.com>
2024-05-17 21:33:26 +0530

    added

--- frontend/src/components/Footer/Footer.jsx
+++ frontend/src/components/Footer/Footer.jsx
@@ -6,12 +6,12 @@ const Footer = () => {
         <div className="footer" id="footer">
             <div className="footer-content">
                 <div className="footer-content-left">
-                    <img src={assets.logo} alt="" />
+                    <img src={assets.logo} alt="logo" />
                     <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Maxime temporibus eveniet provident! Nesciunt, ducimus maxime? Vel, totam voluptatem? Sequi adipisci optio accusamus provident animi quo ea non obcaecati tenetur consectetur.</p>
                     <div className="footer-social-icons">
-                        <img src={assets.facebook_icon} alt="" />
-                        <img src={assets.twitter_icon} alt="" />
-                        <img src={assets.linkedin_icon} alt="" />
+                        <img src={assets.facebook_icon} alt="facebook icon" />
+                        <img src={assets.twitter_icon} alt="twitter icon" />
+                        <img src={assets.linkedin_icon} alt="linkedin icon" />
                     </div>
                 </div>
                 <div className="footer-content-center">
