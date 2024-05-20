d46e277ddc909cccc838d0bbb8bf920a2c7d1687
Angshu Das <angshudas191@gmail.com>
2024-05-21 01:13:48 +0530

    Errors handled

--- backend/middleware/auth.js
+++ backend/middleware/auth.js
@@ -4,7 +4,7 @@ const authMiddleware = async (req, res, next) =>{
 
     const {token} = req.headers;
     if(!token){
-        return res.join({success:false,message:"Not Authorized Login Again"})
+        return res.json({success:false,message:"Not Authorized Login Again"})
     }
     try{
         const token_decode  = jwt.verify(token,process.env.JWT_SECRET);
--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,11 @@
-import express from "express"
+import express, { application } from "express"
 import cors from "cors"
 import { connectDB } from "./config/db.js"
 import foodRouter from "./routes/foodRoute.js"
 import userRouter from "./routes/userRoute.js"
 import 'dotenv/config'
 import cartRouter from "./routes/cartRoute.js"
+
 
 
 //app config
@@ -22,9 +23,8 @@ connectDB();
 //api endpoints
 app.use("/api/food",foodRouter)
 app.use("/images", express.static('uploads'))
-app.use("/api/user",userRouter)
+app.use("/api/user", userRouter)
 app.use("/api/cart", cartRouter)
-
 
 app.get("/", (req, res) => {
     res.send("API WORKING")
