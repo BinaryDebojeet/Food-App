8e0f63169341c659ca0fede4ed80ea111b82dafb
BinaryDebojeet <142610446+BinaryDebojeet@users.noreply.github.com>
2024-05-21 23:42:28 +0530

    added

--- frontend/src/pages/PlaceOrder/PlaceOrder.jsx
+++ frontend/src/pages/PlaceOrder/PlaceOrder.jsx
@@ -1,7 +1,8 @@
-import React, { useContext, useState } from "react";
+import React, { useContext, useEffect, useState } from "react";
 import "./PlaceOrder.css";
 import { StoreContext } from "../../Context/StoreContext";
 import axios from "axios";
+import { useNavigate } from "react-router-dom";
 
 export default function () {
 
@@ -51,7 +52,15 @@ export default function () {
       console.log(response)
     }
   }
-
+const navigate = useNavigate();
+  useEffect(()=>{
+    if(token){
+      navigate('/cart')
+    }
+    else if(getTotalCartAmount()===0){
+      navigate('/cart')
+    }
+  },[token])
 
   return (
     <form onSubmit={placeOrder} className="place-order">
